8c8e9a193ac4f861cf311f101d5e7135a485ab6a
whtfckV <104555352+whtfckV@users.noreply.github.com>
2024-04-18 01:51:18 +0700

    added semantic quotes

--- src/js/script.js
+++ src/js/script.js
@@ -8,21 +8,34 @@
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        const quoteTitle = document.createElement('h3');
-        const quoteAuthor = document.createElement('span');
-        const quote = document.createElement('li');
+//         <figure class="quote">
+        //   <blockquote>
+        //     But web browsers aren’t like web servers. If your back-end code is getting so big that it’s starting to run noticably slowly, you can throw more computing power at it by scaling up your server. That’s not an option on the front-end where you don’t really have one run-time environment—your end users have their own run-time environment with its own constraints around computing power and network connectivity.
+        //   </blockquote>
+        //   <figcaption>
+      //     — Jeremy Keith, <cite>Mental models</cite>
+        //   </figcaption>
+        // </figure>
+        const quote = document.createElement('figure');
+        // const quoteBlock = document.createElement('blockquote');
+        const quoteTitle = document.createElement('blockquote');
+        const quoteAuthor = document.createElement('figcaption');
+        const item = document.createElement('li');
 
+        quote.classList.add('quote__block');
         quoteTitle.classList.add('quote__title');
         quoteAuthor.classList.add('quote__author');
-        quote.append(quoteTitle, quoteAuthor);
+        quote.append(quoteTitle, quoteAuthor)
+        item.append(quote);
 
+        console.log(data.quoteAuthor);
         quoteTitle.textContent = data.quoteText;
         quoteAuthor.innerHTML = `&copy; ${data.quoteAuthor || 'Неивестный автор'}`;
 
         if (quotesList.children.length === 0) {
-          quotesList.append(quote);
+          quotesList.append(item);
         } else {
-          quotesList.insertBefore(quote, quotesList.firstChild);
+          quotesList.insertBefore(item, quotesList.firstChild);
         }
       })
       .catch(error => console.log(error));

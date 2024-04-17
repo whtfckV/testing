f6e1b09c2c585eb7ed53a73e2eb5b460fa64abcc
whtfckV <104555352+whtfckV@users.noreply.github.com>
2024-04-18 01:51:34 +0700

    added semantic quotes

--- src/js/script.js
+++ src/js/script.js
@@ -8,16 +8,7 @@
     fetch(url)
       .then(response => response.json())
       .then(data => {
-//         <figure class="quote">
-        //   <blockquote>
-        //     But web browsers aren’t like web servers. If your back-end code is getting so big that it’s starting to run noticably slowly, you can throw more computing power at it by scaling up your server. That’s not an option on the front-end where you don’t really have one run-time environment—your end users have their own run-time environment with its own constraints around computing power and network connectivity.
-        //   </blockquote>
-        //   <figcaption>
-      //     — Jeremy Keith, <cite>Mental models</cite>
-        //   </figcaption>
-        // </figure>
         const quote = document.createElement('figure');
-        // const quoteBlock = document.createElement('blockquote');
         const quoteTitle = document.createElement('blockquote');
         const quoteAuthor = document.createElement('figcaption');
         const item = document.createElement('li');
